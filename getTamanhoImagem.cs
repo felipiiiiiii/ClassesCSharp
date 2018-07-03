214c54b7a43734f7d7aeaffe06cb748ea1694309
FelipiSantos <gmininsk@gmail.com>
2018-07-02 22:18:48 -0300

    Primeira versão

---
+++ getTamanhoImagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class getTamanhoImagem
+{
+    public static int alturaImg(string diretorioImg)
+    {
+        System.Drawing.Image img = System.Drawing.Image.FromFile(diretorioImg);
+        return img.Height;
+    }
+
+    public static int larguraImg(string diretorioImg)
+    {
+        System.Drawing.Image img = System.Drawing.Image.FromFile(diretorioImg);
+        return img.Width;
+    }
+}
+
