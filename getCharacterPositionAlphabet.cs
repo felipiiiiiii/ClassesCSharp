b29326655360b0239be4061afb6bcfe6ddb25b47
FelipiSantos <gmininsk@gmail.com>
2018-07-02 22:30:09 -0300

    Primeira versão do Posição da letra no alfabeto

---
+++ getCharacterPositionAlphabet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Character
+{
+    public uint getCharacteresPositionOfAlphabet (string a)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        foreach (char c in a.ToLower())
+        {
+            alphabet.IndexOf(c);
+        }
+    }
+}
+
